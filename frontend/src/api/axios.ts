bfd677160b4b5da499ccb5ccf1d3e7f34f3f93df
Sabyasachi1996 <tripathy014@gmail.com>
2026-04-01 17:52:54 +0530

    axios call in frontend fixed. static token was getting passed previously

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -4,9 +4,8 @@ const api = axios.create({
     baseURL: import.meta.env.API_URL || "http://localhost:3000"
 });
 api.interceptors.request.use((config)=>{
-    // const params = new URLSearchParams(window.location.search);
-    //we will remove this once it is properly integrated with flutter TODO:
-    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2M2ZiMzkyNy0xZDVmLTRkMzQtYTllZC00NzcyNWQ3NzEzZTkiLCJ1c2VyRW1haWwiOiJ0cmlwYXRoeTAxNEBnbWFpbC5jb20iLCJ1c2VyUGhvbmUiOiI2Mjk0MTk5MTgxIiwiaWF0IjoxNzc1MDI2MTI5LCJleHAiOjE3NzUwODAxMjl9.JBI_MqT84rmr4IeS0sg11g4HUfrA4HMPwP0sykceJHg"; //params.get("token") || localStorage.getItem("token");
+    const params = new URLSearchParams(window.location.search);    
+    const token = params.get("token") || localStorage.getItem("token");
     if(token) config.headers.Authorization = `Bearer ${token}`;
     return config;
 });
