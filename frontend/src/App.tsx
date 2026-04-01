f7982e3b7e20b555c6c814b330129857aa743cf8
Sabyasachi1996 <tripathy014@gmail.com>
2026-04-01 18:20:08 +0530

    some modifications added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,17 @@
-// import { Toaster } from 'react-hot-toast'
-// import './App.css'
-// import { SubscriptionPage } from './pages/SubscriptionPage'
-
-// function App() {
-//   return (    
-//     <div className="min-h-screen bg-gray-50 flex items-center justify-center">
-//       <Toaster position="top-center" reverseOrder={false} />
-//       <SubscriptionPage />
-//     </div>
-//   )
-// }
-
-// export default App
 import { Toaster } from 'react-hot-toast'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import { SubscriptionPage } from './pages/SubscriptionPage'
 
 function App() {
   return (
-    <div className="min-h-screen bg-stone-50 flex items-center justify-center">
+    <div className="min-h-screen bg-stone-50">
       <Toaster position="top-center" reverseOrder={false} />
-      <SubscriptionPage />
+      <Routes>
+        <Route path="/plans" element={<SubscriptionPage />} />
+      </Routes>
     </div>
   )
 }
 
 export default App
-
